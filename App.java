c98e4d375aef79879b1fa2bed275d4dfcf4fa243
ucdtomaszsadowy <tomasz.sadowy@ucdconnect.ie>
2023-06-25 22:00:54 +0100

    Basic Student Details Commit

--- App.java
+++ App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        //TODO
+        Email student1 = new Email();
+        System.out.print("\n");
+        System.out.println("------Student Details------");
+        System.out.println("Name: " +student1.getFirstName()+ " " + student1.getLastName());
+        System.out.println("College: " +student1.getCollege());
     }
 }
--- Email.java
+++ Email.java
@@ -1,8 +1,78 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
     private String password;
-    private String school;
+    private String college;
     private String altEmail;
     private int maxCapacity;
+
+    public Email(){
+        this.firstName = setFirstName();
+        this.lastName = setLastName();
+        //System.out.println("Name: " +firstName+ " "+ lastName);
+
+        this.college = setCollege(); 
+        //System.out.println("College: "+ college);
+    }
+
+    //Asking student for details
+
+    //First Name Setter
+    private String setFirstName(){
+        System.out.print("Enter your First Name: ");
+        Scanner in = new Scanner(System.in);
+        String studentFirstName = in.nextLine();
+        System.out.print("\n");
+
+        return studentFirstName;
+    }
+
+    //First Name Getter
+    String getFirstName(){
+        return firstName;
+    }
+
+    //Last Name Setter
+    private String setLastName(){
+        System.out.print("Enter your Last Name: ");
+        Scanner in = new Scanner(System.in);
+        String studentLastName = in.nextLine();
+        System.out.print("\n");
+
+        return studentLastName;
+    }
+
+    //Last Name Getter
+    String getLastName(){
+        return lastName;
+    }
+
+    //College Setter
+    private String setCollege(){
+        System.out.print("Please enter your UCD College: Science, Engineering, Business or Health: ");
+        Scanner in = new Scanner(System.in);
+        String userCollege = in.nextLine();
+
+        if (userCollege.matches("Science") || userCollege.matches("science")){
+            return "Science";
+        }
+        else if (userCollege.matches("Engineering") || userCollege.matches("engineering")){
+            return "Engineering";
+        }
+        else if (userCollege.matches("Business") || userCollege.matches("business")){
+            return "Business";
+        }
+        else if (userCollege.matches("Health") || userCollege.matches("health")){
+            return "Health";
+        }
+        else{
+            return "Invalid College";
+        }
+    }
+
+    String getCollege(){
+        return college;
+    }
 }
