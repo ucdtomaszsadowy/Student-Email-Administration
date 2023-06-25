4f0528c278b4cf3f11619f7681522583c1f888ac
ucdtomaszsadowy <tomasz.sadowy@ucdconnect.ie>
2023-06-25 21:03:13 +0100

    Basic Template Commit

---
+++ Email.java
@@ -0,0 +1,8 @@
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String school;
+    private String altEmail;
+    private int maxCapacity;
+}
